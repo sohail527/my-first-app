7827bf0305ccecaf071f687bd48b813e35553e9e
sohail527 <sohail527@users.noreply.github.com>
2020-09-27 15:22:39 +0500

    fifth_commit

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView result;
+    EditText num1 , num2;
+    Button b1 , b2 , b4 , b5 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*result = (TextView)findViewById(R.id.result);
+
+        num1=(EditText)findViewById(R.id.num1);
+        num2=(EditText)findViewById(R.id.num2);
+
+        b1=(Button)findViewById(R.id.b1);
+        b2=(Button)findViewById(R.id.b2);
+        b4=(Button)findViewById(R.id.b4);
+        b5=(Button)findViewById(R.id.b5);*/
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  num1=Integer.ParseInt()
+
+
+            }
+        });
     }
+
+
 }
